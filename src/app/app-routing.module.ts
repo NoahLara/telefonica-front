60798844636be5c3ce7d1726eb85392ada5c8cdd
Noah <noe_lara99@outlook.com>
2022-06-12 13:34:51 -0600

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileCustomerComponent } from './pages/profile-customer/profile-customer.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'sign-in', component: SignInComponent },
+  { path:'sign-up', component: SignUpComponent },
+  { path:'sign-up', component: SignUpComponent },
+  { path:'customers', component: CustomersComponent },
+  { path:'profile-customer', component: ProfileCustomerComponent },
+  { path:'users', component: UsersComponent },
+  { path:'users', component: UsersComponent },
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { ProfileCustomerComponent } from './pages/profile-customer/profile-customer.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ProfileUserComponent } from './pages/profile-user/profile-user.component';
+import { UsersComponent } from './pages/users/users.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    CustomersComponent,
+    ProfileCustomerComponent,
+    SignUpComponent,
+    ProfileUserComponent,
+    UsersComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
